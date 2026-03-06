27e322808eb0aef0f8fcc63e779b827483c095dd
Melky Tobias <melkytobias10@gmail.com>
2026-03-06 15:46:18 -0500

    home

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // ← cambia '/educArte/' por '/'
+  base: '/',  // ← dominio propio = raíz
 })
